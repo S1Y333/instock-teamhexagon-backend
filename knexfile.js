ae0d00de45d1bb06889146ec1d342d706e66c0ba
siyi zhang <siyizhang@siyis-MBP.ht.home>
2024-04-17 14:21:10 -0400

    knex file and migration, seed set up

--- knexfile.js
+++ knexfile.js
@@ -8,8 +8,11 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: process.env.DB_LOCAL_HOST,
-    database: process.env.DB_LOCAL_NAME,
+    database: process.env.DB_LOCAL_DBNAME,
     user: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASSWORD,
+    charset: "utf8",
   },
 };
+
+
