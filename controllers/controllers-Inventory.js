5d0e7e96fb74fb32238c3e3fac0e095f1c162066
Sheela Hamdard <sheela.hamdard@gmail.com>
2024-04-18 17:06:26 -0400

    added warehouse_name to inventory schema via knex

--- controllers/controllers-Inventory.js
+++ controllers/controllers-Inventory.js
@@ -1,14 +1,13 @@
-const knex = require("knex")(require("../knexfile"));
-
 exports.getAllInventoryItems = function (req, res) {
   knex("inventories")
-    .select("*")
+    .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
+    .select("inventories.*", "warehouses.warehouse_name")
     .then((inventories) => {
-      console.log("Fetched inventories:", inventories);
+      console.log("Fetched inventories with warehouse names:", inventories);
       res.status(200).json(inventories);
     })
     .catch((err) => {
-      console.error("Error fetching inventories:", err);
+      console.error("Error fetching inventories with warehouse names:", err);
       res.status(500).send({ error: err.message });
     });
 };
@@ -20,11 +19,20 @@ exports.getInventoryItemById = function (req, res) {
     return res.status(400).send("Invalid ID format");
   }
   knex("inventories")
-    .where({ id: id })
+    .join("warehouses", "inventories.warehouse_id", "=", "warehouses.id")
+    .where("inventories.id", id)
     .first()
+    .select("inventories.*", "warehouses.warehouse_name")
     .then((inventory) => {
-      if (inventory) res.status(200).json(inventory);
-      else res.status(404).send("Inventory item not found");
+      if (inventory) {
+        console.log("Fetched inventory item with warehouse name:", inventory);
+        res.status(200).json(inventory);
+      } else {
+        res.status(404).send("Inventory item not found");
+      }
     })
-    .catch((err) => res.status(500).send({ error: err.message }));
+    .catch((err) => {
+      console.error("Error fetching inventory item with warehouse name:", err);
+      res.status(500).send({ error: err.message });
+    });
 };
