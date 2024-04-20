da64bb7848738d3158bc1c1ed68440f863d7f2e6
siyi zhang <siyizhang@siyis-MBP.ht.home>
2024-04-20 18:50:31 -0400

    backend add put/edit route for inventory 1st round of testing pass

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const controllersInventory = require("../controllers/controllers-Inventory");
 
 router.get("/", controllersInventory.getAllInventoryItems);
-
+router.get("/:id", controllersInventory.getInventoryItemById);
 router.post("/", controllersInventory.createInventoryItem);
 router.delete("/:id", controllersInventory.deleteInventoryItem);
-
+router.put("/:id", controllersInventory.updateInventoryItem);
 
 
 module.exports = router;
